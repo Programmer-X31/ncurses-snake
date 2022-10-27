27fcb25bfac237b1a9547c81b2933c643acfcbb3
Programmer-X31 <sachinsamurai@gmail.com>
2022-10-27 07:03:36 +0530

    Made the shellscript more safe and Updated README

--- ex.sh
+++ ex.sh
@@ -1,9 +1,11 @@
+mkdir -p bin/obj 2>> /dev/null
+
 echo "Compiling snake.c into objectfile..."
-gcc -c src/snake.c -o bin/obj/snake.o
+gcc -c -lncursesw src/snake.c -o bin/obj/snake.o
 echo "Compiling main.c into objectfile..."
-gcc -c src/main.c -o bin/obj/main.o
+gcc -c -lncursesw src/main.c -o bin/obj/main.o
 echo "Compiling food.c into objectfile..."
-gcc -c src/food.c -o bin/obj/food.o
+gcc -c -lncursesw src/food.c -o bin/obj/food.o
 echo "Linking Objectfiles"
 gcc -lncursesw -Wall -std=c99 -lm bin/obj/main.o bin/obj/snake.o bin/obj/food.o -o bin/snake-game
 
